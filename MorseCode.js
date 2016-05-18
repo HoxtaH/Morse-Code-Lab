82e7f8249b1a810b0d2f339c0a9ef4c659c32966
HoxtaH <tyrellhoxter@gmail.com>
2016-05-18 11:13:23 -0400

    first commit

---
+++ MorseCode.js
@@ -0,0 +1,55 @@
+var message = prompt("Enter a message");
+message = message.toLowerCase();
+var alphabets = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "-": "-....-",
+    " ": "/"
+}
+  
+
+    var translation = "";
+    for(var i = 0; i < message.length; i++){
+        var character = message.charAt(i);
+        translation += alphabets[character];
+    }
+
+document.getElementById("result").innerHTML = translation;
+
